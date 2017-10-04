4bd8f8e885e89b2a4e6a2eeb8faf38cb4253c89c
John Divelbiss <john.divelbiss@foundant.com>
2017-10-04 10:08:53 -0600

    fixes

--- install.pl
+++ install.pl
@@ -1,8 +1,30 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-use Getopt::Long;
+#!/usr/bin/env perl
+use Getopt::Long 'HelpMessage';
+
+#=================================================================
+#   HELP MENU
+
+=head1 NAME
+
+install.pl - installs environment scripts to PC
+
+=head1 SYNOPSIS
+
+  --help, -h        Displays Help Menu (this)
+  --verbose, -v     Displays debug information
+  --package, -p     Packages to install (will install all if not provided)
+
+=head1 VERSION
+
+1.0
+
+=cut
+# END HELP MENU
+#=================================================================
 
 GetOptions(
+    'help'      => sub { HelpMessage(0) },
+    'verbose' => \my $verbose,
+    'package' => \(my $package = 'all'),
+) or HelpMessage(1);
 
-) or die "Invalid options passed to $0\n";
